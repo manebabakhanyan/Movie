6bc504694dd4776fab0652ca6388889c20b3aedb
Mane <manebabakhanyan11.ait@gmail.com>
2024-02-09 17:32:58 +0400

    Changed populars

--- src/Components/Home/Movie/MovieContext/PopularMovieContext.jsx
+++ src/Components/Home/Movie/MovieContext/PopularMovieContext.jsx
@@ -3,18 +3,18 @@ import React, { createContext, useContext, useState } from 'react';
 const MovieContext = createContext();
 
 export function usePopularMovieContext() {
-    return useContext(MovieContext);
+    return useContext(MovieContext)
 }
 
 export function PopularMovieProvider({ children }) {
     const [currentIndex, setCurrentIndex] = useState(0);
     function forward() {
-        setCurrentIndex(prevIndex => prevIndex - 1);
-    };
-
+        setCurrentIndex(prevIndex => Math.max(prevIndex - 1, 0))
+    }
+    
     function backword() {
         setCurrentIndex(prevIndex => Math.max(prevIndex + 1));
-    };
+    }
 
     return (
         <MovieContext.Provider value={{ currentIndex, forward, backword }}>
