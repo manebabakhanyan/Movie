a082d3ce56d67c4762cd1a7660efd7596b715633
Mane <manebabakhanyan11.ait@gmail.com>
2024-02-15 03:28:56 +0400

    Liked movies count

--- src/Components/Home/Header/Header.jsx
+++ src/Components/Home/Header/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../Header/Logo/Logo (6).png'
+import Logo from '../Header/Logo/Logo (6).png';
 import Search from './Search';
+import useFavouriteFilms from '../../../Store/useFavouriteFilms';
+
 export default function Header() {
+    const { favouriteCount } = useFavouriteFilms();
+
     return (
         <div className='flex justify-between pt-[50px] pb-[50px] md550:flex-col md800:flex-row md700:ml-[70px]'>
             <Link to='/'>
@@ -10,12 +14,18 @@ export default function Header() {
             </Link>
             <div className='flex items-center gap-[20px] md550:flex-col md550:mt-[20px] md600:flex-row md600:ml-[60px] md800:mt-[-5px]'>
                 <Link to='/favourite'>
-                    <i className='far fa-heart cursor-pointer w-[50px]'></i>
+                    <div className="relative">
+                        <i className='far fa-heart cursor-pointer w-[50px]'></i>
+                        {favouriteCount > 0 && (
+                            <div className="absolute -top-1 right-[14px] bg-red text-white text-[13px] rounded-full h-[15px] w-[15px] flex items-center justify-center">
+                                {favouriteCount}
+                            </div>
+                        )}
+                    </div>
                 </Link>
 
                 <Search />
-
             </div>
         </div>
     );
-};
+}
--- src/Store/useFavouriteFilms.js
+++ src/Store/useFavouriteFilms.js
@@ -1,15 +1,21 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
 const useFavouriteFilms = create((set) => ({
-    favouriteFilms: [],
-    setFavouriteFilms: (films) => set({ favouriteFilms: films }),
+    favouriteFilms: JSON.parse(localStorage.getItem('favouriteFilms')) || [],
+    favouriteCount: JSON.parse(localStorage.getItem('favouriteFilms'))?.length || 0,
+    setFavouriteFilms: (films) => {
+        localStorage.setItem('favouriteFilms', JSON.stringify(films));
+        set({ favouriteFilms: films, favouriteCount: films.length });
+    },
     addFilm: (movie) => {
-        set((state) => ({ favouriteFilms: [...state.favouriteFilms, movie] }));
+        const updatedFilms = [...JSON.parse(localStorage.getItem('favouriteFilms')) || [], movie];
+        localStorage.setItem('favouriteFilms', JSON.stringify(updatedFilms));
+        set({ favouriteFilms: updatedFilms, favouriteCount: updatedFilms.length });
     },
     deleteFilm: (filmId) => {
-        set((state) => ({
-            favouriteFilms: state.favouriteFilms.filter(film => film && film.id && film.id !== filmId)
-        }));
+        const updatedFilms = JSON.parse(localStorage.getItem('favouriteFilms')).filter(film => film.id !== filmId);
+        localStorage.setItem('favouriteFilms', JSON.stringify(updatedFilms));
+        set({ favouriteFilms: updatedFilms, favouriteCount: updatedFilms.length });
     },
 }));
 
